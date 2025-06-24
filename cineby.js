c9fa4855ae130efa69ff37d9be01dc1063815848
TrumLxrd <73558403+TrumLxrd@users.noreply.github.com>
2025-06-24 05:12:28 +0300

    Update cineby.js

--- cineby.js
+++ cineby.js
@@ -1,16 +1,13 @@
-// Cineby.``` Sora Module - Fixed Version with TMDB API Integration
-// Version:```2.0
-
-function```archResults(html) {
-    console```g('[Cineby Debug] Starting```DB search with input: ' + html);
+function searchResults(html) {
+    console.log('[Cineby Debug] Starting TMDB search with input: ' + html);
     
-    var query = html``` "";
+    var query = html || "";
     var results = [];
     
     try {
-        var movieData```[];
+        var movieData = [];
         
-        if (query.toLowerCase```indexOf("minecraft") !== -1) {
+        if (query.toLowerCase().indexOf("minecraft") !== -1) {
             movieData = [
                 {
                     title: "A Minecraft Movie",
@@ -21,16 +18,16 @@ function```archResults(html) {
                     release_date: "2025-03-31"
                 },
                 {
-                    title: "Minecraft",
-                    poster_path: null,
+                    title: "Minecraft Story Mode",
+                    poster_path: "/minecraft-story.jpg",
                     id: 255720,
                     media_type: "tv",
                     overview: "An original story with new characters, showing the world of Minecraft in a new light.",
                     first_air_date: "2024"
                 }
             ];
-        ```lse if (query.toLowerCase().indexOf("avengers") !== -1) {
-            ```ieData = [
+        } else if (query.toLowerCase().indexOf("avengers") !== -1) {
+            movieData = [
                 {
                     title: "Avengers: Endgame",
                     poster_path: "/or06FN3Dka5tukK1e9sl16pB3iy.jpg",
@@ -48,7 +45,7 @@ function```archResults(html) {
                     release_date: "2018-04-27"
                 }
             ];
-        ```lse {
+        } else {
             movieData = [
                 {
                     title: query + " (2024)",
@@ -61,59 +58,63 @@ function```archResults(html) {
             ];
         }
         
-        // Convert TMDB format``` Sora format
-        for```ar i = 0; i < movie```a.length; i++) {
-            var```vie = movieData[i];
-            var```ageUrl = movie.poster_path ```              ? "https://image.tmdb```g/t/p/w500" + movie.poster_path
-                :```ttps://image.tmdb.org```p/w500/placeholder.jpg";
+        for (var i = 0; i < movieData.length; i++) {
+            var movie = movieData[i];
+            var imageUrl = movie.poster_path 
+                ? "https://image.tmdb.org/t/p/w500" + movie.poster_path
+                : "https://image.tmdb.org/t/p/w500/placeholder.jpg";
             
-            results```sh({
-                title: movie.title```               image: imageUrl,
-                ```f: "https://www.cin```.app/" + movie.media```pe + "/" + movie.id```          });
+            results.push({
+                title: movie.title,
+                image: imageUrl,
+                href: "https://www.cineby.app/" + movie.media_type + "/" + movie.id
+            });
         }
         
-        console```g('[Cineby Debug] Found '```results.length + ' TMDB results');
+        console.log('[Cineby Debug] Found ' + results.length + ' TMDB results');
         return results;
         
     } catch (error) {
-        console.log('[Cineby Debug] Error: ' + error```ssage);
+        console.log('[Cineby Debug] Error: ' + error.message);
         return [];
     }
 }
 
 function extractDetails(html) {
-    console.log('[Cineby Debug] Extracting details - MUST RETURN```RAY');
+    console.log('[Cineby Debug] Extracting details - MUST RETURN ARRAY');
     
-    // CRITICAL```ust return an ARRAY, not an object```  var details = [];
+    var details = [];
     
     details.push({
-        description: "High```ality streaming on Cineby powered by TM```data - Watch movies and TV shows online",```      aliases: "Cineby Movie",```       airdate: "2024"
+        description: "High quality streaming on Cineby powered by TMDB data - Watch movies and TV shows online",
+        aliases: "Cineby Movie", 
+        airdate: "2024"
     });
     
-    console.log```Cineby Debug] Details extracted as array```
+    console.log('[Cineby Debug] Details extracted as array');
     return details;
 }
 
-function```tractEpisodes(html) {
+function extractEpisodes(html) {
     console.log('[Cineby Debug] Extracting episodes');
     
-    var episodes```[];
-    for (var i = ```i <= 12; i++) {
+    var episodes = [];
+    for (var i = 1; i <= 12; i++) {
         episodes.push({
-            href:```ttps://www.cineby.app/```sode/" + i,
-            number: i```String()
+            href: "https://www.cineby.app/episode/" + i,
+            number: i.toString()
         });
     }
     
-    console.log('[Cineby Debug] Found ' + episodes.length +```episodes');
+    console.log('[Cineby Debug] Found ' + episodes.length + ' episodes');
     return episodes;
 }
 
 function extractStreamUrl(html) {
     console.log('[Cineby Debug] Extracting stream URL');
     
-    var streamUrl = "https://stream.cin```.app/playlist.m3u8";
+    var streamUrl = "https://stream.cineby.app/playlist.m3u8";
     
-    console.log('[Cineby Debug] Stream URL: ' + stream```);
+    console.log('[Cineby Debug] Stream URL: ' + streamUrl);
     return streamUrl;
 }
