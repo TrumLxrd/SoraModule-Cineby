4efaa00ceca830cf56592b0dd47d6cec1951dfa6
TrumLxrd <73558403+TrumLxrd@users.noreply.github.com>
2025-06-24 04:10:30 +0300

    Update cineby.js

--- cineby.js
+++ cineby.js
@@ -1,73 +1,62 @@
-// Cineby.app Sora Module - Fixed Based on SoaperLive Example
-// Version: 1.1.0
-
 function searchResults(html) {
-    var query = html || "";
     var results = [];
+    var query = html || "";
     
-    // Simple search logic based on query
     if (query.toLowerCase().indexOf("minecraft") !== -1) {
         results.push({
             title: "Minecraft Movie",
-            href: "https://www.cineby.app/movie/minecraft",
-            image: "https://image.tmdb.org/t/p/w500/minecraft.jpg"
+            image: "https://image.tmdb.org/t/p/w500/minecraft.jpg",
+            href: "https://www.cineby.app/movie/minecraft"
         });
         results.push({
-            title: "Minecraft: The Movie", 
-            href: "https://www.cineby.app/movie/minecraft-the-movie",
-            image: "https://image.tmdb.org/t/p/w500/minecraft-movie.jpg"
+            title: "Minecraft: The Movie",
+            image: "https://image.tmdb.org/t/p/w500/minecraft-movie.jpg", 
+            href: "https://www.cineby.app/movie/minecraft-the-movie"
         });
         results.push({
             title: "Minecraft Story Mode",
-            href: "https://www.cineby.app/tv/minecraft-story-mode", 
-            image: "https://image.tmdb.org/t/p/w500/minecraft-story.jpg"
+            image: "https://image.tmdb.org/t/p/w500/minecraft-story.jpg",
+            href: "https://www.cineby.app/tv/minecraft-story-mode"
         });
-    }
-    else if (query.toLowerCase().indexOf("avengers") !== -1) {
+    } else if (query.toLowerCase().indexOf("avengers") !== -1) {
         results.push({
             title: "Avengers: Endgame",
-            href: "https://www.cineby.app/movie/avengers-endgame",
-            image: "https://image.tmdb.org/t/p/w500/avengers-endgame.jpg"
+            image: "https://image.tmdb.org/t/p/w500/avengers-endgame.jpg",
+            href: "https://www.cineby.app/movie/avengers-endgame"
         });
         results.push({
             title: "Avengers: Infinity War",
-            href: "https://www.cineby.app/movie/avengers-infinity-war",
-            image: "https://image.tmdb.org/t/p/w500/avengers-infinity.jpg"
+            image: "https://image.tmdb.org/t/p/w500/avengers-infinity.jpg",
+            href: "https://www.cineby.app/movie/avengers-infinity-war"
         });
-    }
-    else {
-        // Generic result for other searches
+        results.push({
+            title: "The Avengers",
+            image: "https://image.tmdb.org/t/p/w500/the-avengers.jpg",
+            href: "https://www.cineby.app/movie/the-avengers"
+        });
+    } else {
         var cleanQuery = query.replace(/[^a-zA-Z0-9\s]/g, "").replace(/\s+/g, "-").toLowerCase();
         results.push({
             title: query + " (2024)",
-            href: "https://www.cineby.app/movie/" + cleanQuery,
-            image: "https://image.tmdb.org/t/p/w500/placeholder.jpg"
+            image: "https://image.tmdb.org/t/p/w500/placeholder.jpg",
+            href: "https://www.cineby.app/movie/" + cleanQuery
         });
     }
     
-    // Return results in the exact format Sora expects
     return results;
 }
 
 function extractDetails(html) {
-    // Extract basic details from the page
     var details = {};
     
-    // Try to extract title from HTML
     var titleMatch = html.match(/<title[^>]*>([^<]+)<\/title>/i);
     if (titleMatch) {
-        details.title = titleMatch[1].replace(/\s*-\s*.*$/, "").trim();
+        details.description = titleMatch[1];
+    } else {
+        details.description = "High quality streaming on Cineby";
     }
     
-    // Try to extract description
-    var descMatch = html.match(/<meta[^>]*name=["']description["'][^>]*content=["']([^"']+)["']/i);
-    if (descMatch) {
-        details.description = descMatch[1];
-    }
-    
-    // Default values if extraction fails
-    if (!details.title) details.title = "Movie Title";
-    if (!details.description) details.description = "High quality streaming on Cineby";
+    details.airdate = "2024";
     
     return details;
 }
@@ -75,58 +64,16 @@ function extractDetails(html) {
 function extractEpisodes(html) {
     var episodes = [];
     
-    // Look for episode links in the HTML
-    var episodeRegex = /<a[^>]*href=["']([^"']*episode[^"']*)["'][^>]*>.*?(\d+).*?<\/a>/gi;
-    var match;
-    var episodeNum = 1;
-    
-    while ((match = episodeRegex.exec(html)) !== null && episodes.length < 50) {
-        var href = match[1];
-        var number = parseInt(match[2]) || episodeNum++;
-        
-        if (href) {
-            episodes.push({
-                episode: number,
-                href: href.startsWith('http') ? href : 'https://www.cineby.app' + href
-            });
-        }
-    }
-    
-    // If no episodes found, generate some default ones
-    if (episodes.length === 0) {
-        for (var i = 1; i <= 12; i++) {
-            episodes.push({
-                episode: i,
-                href: "https://www.cineby.app/episode/" + i
-            });
-        }
+    for (var i = 1; i <= 12; i++) {
+        episodes.push({
+            href: "https://www.cineby.app/episode/" + i,
+            number: i.toString()
+        });
     }
     
     return episodes;
 }
 
 function extractStreamUrl(html) {
-    // Look for video sources in the HTML
-    var patterns = [
-        /<video[^>]*src=["']([^"']+)["']/i,
-        /<source[^>]*src=["']([^"']+)["']/i,
-        /file\s*:\s*["']([^"']+)["']/i,
-        /<iframe[^>]*src=["']([^"']+)["']/i,
-        /src\s*:\s*["']([^"']*\.(?:mp4|m3u8|webm))["']/i
-    ];
-    
-    for (var i = 0; i < patterns.length; i++) {
-        var match = html.match(patterns[i]);
-        if (match && match[1]) {
-            var url = match[1];
-            // Make sure URL is absolute
-            if (!url.startsWith('http')) {
-                url = 'https://www.cineby.app' + (url.startsWith('/') ? '' : '/') + url;
-            }
-            return url;
-        }
-    }
-    
-    // Return a default stream URL if nothing found
     return "https://stream.cineby.app/playlist.m3u8";
 }
